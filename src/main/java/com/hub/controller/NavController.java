a88285262020d1f7d68709c39c29feb14508ef23
Krishna Chaitanya <150346887+KRISHNA-TE@users.noreply.github.com>
2024-05-05 11:52:14 +0530

    Add files via upload

---
+++ src/main/java/com/hub/controller/NavController.java
@@ -0,0 +1,24 @@
+package com.hub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NavController
+{
+	@GetMapping("/register")
+	public String registration()
+	{
+		return "Registration";			//HTML File 
+	}
+	@GetMapping("/login")
+	public String login()
+	{
+		return "Login";					//Navigates to the HTML File
+	}
+	@GetMapping("/addsong")
+	public String song() {
+		return "song";					//Navigate to the HTML File
+	}
+	
+}
---
+++ src/main/java/com/hub/controller/UserController.java
@@ -0,0 +1,89 @@
+package com.hub.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.hub.entity.Song;
+import com.hub.entity.User;
+import com.hub.service.SongService;
+import com.hub.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class UserController 
+{
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	SongService songService;
+	
+	//Registration_Page
+	@PostMapping("/register")
+	public String adduser( @ModelAttribute User user)
+	{	
+		//To Check a user with a Email is present
+		boolean userExists = userService.emailExists(user);
+		
+		if(userExists==false)
+		{
+			userService.saveuser(user);
+			System.out.println("User Added Sucessfully");
+		}
+		else
+		{
+			System.out.println("Duplicate User");
+		}
+		return "login";
+	}
+	
+	//Login_Page
+	@PostMapping("/validate")
+	public String validate( @RequestParam ("email")String email,@RequestParam("password") String password, HttpSession session,Model model)
+	{				
+		if(userService.validUser(email,password)==true)
+		{
+			
+			session.setAttribute("email", email);
+			
+			String role = userService.getRole(email);
+			
+			if(role.equals("admin"))
+			{
+				return "adminhome";
+			}
+			else
+			{
+				User user = userService.getUser(email);
+				boolean userstatus = user.isPremium();
+				
+				List<Song> fetchAllSongs = songService.fetchAllSongs();
+				model.addAttribute("songs",fetchAllSongs);
+				
+				model.addAttribute("ispremium", userstatus);
+				return "customerhome";
+			}
+		}
+		else
+		{
+			return "login";
+		}		
+	}
+
+	@GetMapping("/logout") 
+	public String logout(HttpSession session)
+	{
+		session.invalidate();
+		return "login";
+	}
+		
+}
